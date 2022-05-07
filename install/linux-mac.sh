db9f108239614972e445700f6b2de6ba98c4ffbd
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-05-07 11:25:05 +0000

    Installing the script, making it executable
    $dir is not set yet, more thing to be added in the near future

--- install/linux-mac.sh
+++ install/linux-mac.sh
@@ -1,14 +1,18 @@
-# TESTED ON PopOS 21 and 22 LTS
+#!/usr/bin/env bash
+# Distros tested: PopOS 21, 22 LTS
+# Edits by RaptaG (https://github.com/RaptaG)
 
-cd ./
 echo "INFO | Installing dependencies..."
 sudo apt-get install python-tk
 pip3 install -r requirements.txt
-if [ $? -eq 0 ]; then # success
+git clone https://github.com/Fabulously-Optimized/vanilla-installer/ $dir
+cd $dir/vanilla-installer/
+chmod +x gui.py
+if [ $? -eq 0 ]; then # Success
     echo "SUCCESS | Done. Starting the script..."
     python3 vanilla-installer/gui.py
     
-    if [ $? -eq 0 ]; then # success
+    if [ $? -eq 0 ]; then # Failure
         echo "WARNING | Python script terminated!"
     else
         echo "ERROR | Python script had an issue!"
