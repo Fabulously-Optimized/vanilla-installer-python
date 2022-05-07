275cfaaacf4ec87a1befc87db6a9c4eef6403e18
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-05-07 12:34:12 +0000

    New features
    Added:
    
    - Root permission checker
    - Auto install python-tk for many distros and MacOS (untested and maybe not working)
    - Better installing
    - Missing things (some exit commands)
    - Clear descriptions for everything
    
    ($dir will be added in the next commit)

--- install/linux-mac.sh
+++ install/linux-mac.sh
@@ -1,22 +1,60 @@
 #!/usr/bin/env bash
-# Distros tested: PopOS 21, 22 LTS
 # Edits by RaptaG (https://github.com/RaptaG)
 
+# Root permission checker
+if [ $EUID -ne 0 ]; then
+  echo "ERROR | Root permissions are needed for the script to work. Please run sudo ./$(basename $0)" >&2
+  exit 1
+fi
+
+# Installing python-tk, according to the distro/OS and the python requirements
 echo "INFO | Installing dependencies..."
-sudo apt-get install python-tk
+cd /usr/bin/
+if [ls apt apt]; then
+    apt-get install python-tk >/dev/null
+fi
+if [ls pacman pacman]; then
+    pacman -S python-tk >/dev/null
+fi
+if [ls dnf dnf]; then
+     dnf install python-tk >/dev/null
+fi
+if [ls zypper zypper]; then
+    zypper install python-tk >/dev/null
+else
+    brew install python-tk
+fi
 pip3 install -r requirements.txt
-git clone https://github.com/Fabulously-Optimized/vanilla-installer/ $dir
-cd $dir/vanilla-installer/
+
+# Downloading the installer
+echo "INFO | Downloading the installer..."
+git clone https://github.com/Fabulously-Optimized/vanilla-installer /tmp/vi
+
+# Making it executable, moving it to the selected directory
+echo "INFO | Installing the installer..."
+cd /tmp/vi/vanilla-installer/
 chmod +x gui.py
-if [ $? -eq 0 ]; then # Success
+cp gui.py $dir
+
+# Cleaning up
+echo " INFO | Removing unnecessary files..."
+cd /tmp/
+rm -r vi
+
+# Running the script
+if [ $? -eq 0 ]; then # Success output
     echo "SUCCESS | Done. Starting the script..."
-    python3 vanilla-installer/gui.py
-    
-    if [ $? -eq 0 ]; then # Failure
+    cd $dir
+    python3 gui.py
+    if [ $? -eq 0 ]; then # Failure output
         echo "WARNING | Python script terminated!"
+        exit
     else
         echo "ERROR | Python script had an issue!"
+        exit
     fi
 else
     echo "ERROR | Dependencies should not be installed. Exiting."
+    exit
 fi
+exit 1
