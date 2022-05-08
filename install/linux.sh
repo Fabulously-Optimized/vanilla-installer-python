e2a4398d328cd911fefcbf45241652432407fca5
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-05-08 12:49:45 +0000

    Edits
    - $dir is now set in the MC folder
    - Everything will move there
    - Fixed -ne instead of -eq
    - Removed root checker since the root password is automatically asked

--- install/linux.sh
+++ install/linux.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 # Edits by RaptaG (https://github.com/RaptaG)
 
-# Root permission checker
-if [ $EUID -ne 0 ]; then
-  echo "ERROR | Root permissions are needed for the script to work. Please run sudo ./$(basename $0)" >&2
-  exit 1
-fi
-
 # Installing python-tk (according to the distro) and the python requirements
 echo "INFO | Installing dependencies..."
 cd /usr/bin/
 if [ls apt -eq apt]; then
-    apt-get install python-tk
+    sudo apt-get install python-tk
 else
   if [ls pacman -eq pacman]; then
-      pacman -S python-tk
+    sudo pacman -S python-tk
 else
   if [ls dnf -eq dnf]; then
-     dnf install python-tk
+    sudo dnf install python-tk
 else
   if [ls zypper -eq zypper]; then
-      zypper install python-tk
+    sudo zypper install python-tk
 fi
 pip3 install -r requirements.txt
 
@@ -28,29 +22,36 @@ pip3 install -r requirements.txt
 echo "INFO | Downloading the installer..."
 git clone https://github.com/Fabulously-Optimized/vanilla-installer /tmp/vi
 
-# Directory selection (default /usr/bin)
-read -p "Enter the directory you want (Press Enter to skip): " dir
+# Directory selection (default ~/.minecraft/)
+read -p "SELECT | Enter the directory you want (Press Enter to skip): " dir
 if [ -z $dir ]; then
-    dir="/usr/bin/"
+    dir="$HOME/.minecraft/"
 fi
 
-# Making it executable, moving it to the selected directory
-echo "INFO | Installing the installer..."
-cd /tmp/vi/vanilla-installer/
-chmod +x gui.py
-cp gui.py $dir
+# Making them executable, moving them to the selected directory
+echo "INFO | Installing the FO vanilla installer..."
+cd $dir
+mkdir vanilla-installer
+cd /tmp/vi/
+mv data $dir/vanilla-installer/data
+mv vanilla-installer $dir/vanilla-installer/installer
+mv media $dir/vanilla-installer/media
+cd $dir/vanilla-installer/installer/
+chmod +x gui.py main.py theme.py
 
 # Cleaning up
 echo "INFO | Removing unnecessary files..."
 cd /tmp/
 rm -rf vi
+cd $dir/vi/media/
+rm -rf screenshots
 
 # Running the script
 if [ $? -eq 0 ]; then # Success output
     echo "SUCCESS | Done. Starting the script..."
-    cd $dir
+    cd $dir/vi/installer
     python3 gui.py
-    if [ $? -eq 0 ]; then # Failure output
+    if [ $? -ne 0 ]; then # Failure output
         echo "WARNING | Python script terminated!"
         exit
     else
