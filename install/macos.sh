a056083ef74256de163ba20386128b0c7a2084d6
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-05-08 12:50:02 +0000

    Edits, matching the latest linux.sh commit

--- install/macos.sh
+++ install/macos.sh
@@ -11,28 +11,35 @@ echo "INFO | Downloading the installer..."
 tmp=$(mktemp -d)
 git clone https://github.com/Fabulously-Optimized/vanilla-installer /$tmp/vi
 
-# Directory selection (default /usr/local/bin)
+# Directory selection (default ~/Library/Application Support/Minecraft/)
 read -p "Enter the directory you want (Press Enter to skip): " dir
 if [ -z $dir ]; then
-    dir="/usr/local/bin/"
+    dir="$HOME/Library/Application\ Support/Minecraft/"
 fi
 
-# Making it executable, moving it to the selected directory
-echo "INFO | Installing the installer..."
-cd /$tmp/vi/vanilla-installer/
-chmod +x gui.py
-cp gui.py $dir
+# Making them executable, moving them to the selected directory
+echo "INFO | Installing the FO vanilla installer..."
+cd $dir
+mkdir vanilla-installer
+cd /$tmp/vi/
+mv data $dir/vanilla-installer/data
+mv vanilla-installer $dir/vanilla-installer/installer
+mv media $dir/vanilla-installer/media
+cd $dir/vanilla-installer/installer/
+chmod +x gui.py main.py theme.py
 
 # Cleaning up
 echo "INFO | Removing unnecessary files..."
 rm -rf $tmp
+cd $dir/vi/media/
+rm -rf screenshots
 
 # Running the script
 if [ $? -eq 0 ]; then # Success output
     echo "SUCCESS | Done. Starting the script..."
     cd $dir
     python3 gui.py
-    if [ $? -eq 0 ]; then # Failure output
+    if [ $? -ne 0 ]; then # Failure output
         echo "WARNING | Python script terminated!"
         exit
     else
