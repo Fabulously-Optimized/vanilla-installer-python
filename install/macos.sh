4ff10d599c449d6f075b2835e2635834d9834291
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-05-09 18:49:09 +0000

    Matching linux.sh latest chanes
    (Also added previous forgotten commits)

--- install/macos.sh
+++ install/macos.sh
@@ -7,47 +7,47 @@ brew install python-tk
 pip3 install -r requirements.txt
 
 # Downloading the installer
-echo "INFO | Downloading the installer..."
+echo "INFO | Downloading the FO Vanilla Installer..."
 tmp=$(mktemp -d)
 git clone https://github.com/Fabulously-Optimized/vanilla-installer /$tmp/vi
 
 # Directory selection (default ~/Library/Application Support/Minecraft/)
-read -p "Enter the directory you want (Press Enter to skip): " dir
+read -p "SELECT | Enter the directory you want (Press Enter to skip): " dir
 if [ -z $dir ]; then
-    dir="$HOME/Library/Application\ Support/Minecraft/"
+    dir="$HOME/Library/Application\ Support/Minecraft"
 fi
 
 # Making them executable, moving them to the selected directory
-echo "INFO | Installing the FO vanilla installer..."
-cd $dir
-mkdir vanilla-installer
+echo "INFO | Installing the FO Vanilla Installer..."
+cd $dir/
+mkdir VanillaInstaller
+cd VanillaInstaller/
+mkdir scripts
 cd /$tmp/vi/
-mv data $dir/vanilla-installer/data
-mv vanilla-installer $dir/vanilla-installer/installer
-mv media $dir/vanilla-installer/media
-cd $dir/vanilla-installer/installer/
+mv data $dir/VanillaInstaller/scripts/data
+mv vanilla-installer $dir/VanillaInstaller/scripts/installer
+mv media $dir/VanillaInstaller/scripts/media
+cd $dir/VanillaInstaller/scripts/installer/
 chmod +x gui.py main.py theme.py
 
 # Cleaning up
 echo "INFO | Removing unnecessary files..."
 rm -rf $tmp
-cd $dir/vi/media/
+cd $dir/VanillaInstaller/scripts/media/
 rm -rf screenshots
 
 # Running the script
 if [ $? -eq 0 ]; then # Success output
     echo "SUCCESS | Done. Starting the script..."
-    cd $dir
+    cd $dir/VanillaInstaller/scripts/installer/
     python3 gui.py
     if [ $? -ne 0 ]; then # Failure output
-        echo "WARNING | Python script terminated!"
-        exit
-    else
-        echo "ERROR | Python script had an issue!"
+        echo "WARNING | Python script had an issue and terminated!"
         exit
     fi
 else
-    echo "ERROR | Dependencies should not be installed. Exiting."
+    echo "ERROR | Dependencies should not be installed. Please run the script again."
+    echo "Exiting..."
     exit
 fi
 exit 1
