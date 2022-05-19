11ad9929a2d19841ba26b07debe056499232152a
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-05-19 12:19:05 +0000

    Yes.

--- install/macos.sh
+++ install/macos.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 # Made by RaptaG (https://github.com/RaptaG)
 
-# Installing python-tk
-echo "INFO | Installing dependencies..."
-brew install python-tk
-
-# Downloading the installer and the python requirements
+# Downloading the installer, installing python-tk and the python requirements
 echo "INFO | Downloading the FO Vanilla Installer..."
 tmp="$(mktemp -d)"
-if [ -d $tmp ]; then
-    rm -rf $tmp
-    git clone https://github.com/Fabulously-Optimized/vanilla-installer /$tmp/fovi
-else
-    git clone https://github.com/Fabulously-Optimized/vanilla-installer /$tmp/fovi
-fi
+git clone https://github.com/Fabulously-Optimized/vanilla-installer /$tmp/fovi
+echo "INFO | Installing dependencies..."
+pip install tk > /dev/null 2>&1
 cd /$tmp/fovi/
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt > /dev/null 2>&1
 
-# Directory selection (default ~/Library/Application Support/Minecraft/)
+# Directory selection (default ~/Library/Application Support/Minecraft)
 read -p "SELECT | Enter the directory you want (Press Enter to skip): " dir
 if [ -z $dir ]; then
     dir="$HOME/Library/Application\ Support/Minecraft"
@@ -30,9 +23,8 @@ mkdir VanillaInstaller
 cd VanillaInstaller/
 mkdir scripts
 cd /$tmp/fovi/
-mv data $dir/VanillaInstaller/scripts/data
-mv vanilla-installer $dir/VanillaInstaller/scripts/installer
-mv media $dir/VanillaInstaller/scripts/media
+mv vanilla-installer installer
+mv data installer media $dir/VanillaInstaller/scripts/
 cd $dir/VanillaInstaller/scripts/installer/
 chmod +x gui.py main.py theme.py
 
@@ -40,19 +32,9 @@ chmod +x gui.py main.py theme.py
 echo "INFO | Removing unnecessary files..."
 rm -rf $tmp
 cd $dir/VanillaInstaller/scripts/media/
-rm -rf screenshots
+rm -r screenshots
 
 # Running the script
-if [ $? -eq 0 ]; then # Success output
-    echo "SUCCESS | Done. Starting the script..."
-    cd $dir/VanillaInstaller/scripts/installer/
-    python3 gui.py
-    if [ $? -ne 0 ]; then # Failure output
-        echo "WARNING | The python script had an issue and terminated!"
-        exit 1
-    fi
-else
-    echo "ERROR | Dependencies should not be installed. Please run the script again."
-    echo "Exiting..."
-    exit 1
-fi
+echo "SUCCESS | Done. Starting the script..."
+cd $dir/VanillaInstaller/scripts/installer/
+python3 gui.py
