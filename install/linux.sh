b72c305473feeb71be8ba2895509cdeb087afd9f
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-06-27 10:11:06 +0300

    Further improvements to the code

--- install/linux.sh
+++ install/linux.sh
@@ -1,40 +1,54 @@
 #!/bin/bash
 # Enhanced by RaptaG (https://github.com/RaptaG)
 
-# Downloading the installer, installing python-tk and the python requirements
-echo "INFO | Downloading the installer..."
-git clone https://github.com/Fabulously-Optimized/vanilla-installer /tmp/fovi
-echo "INFO | Installing dependencies..."
-pip install tk > /dev/null 2>&1
-cd /tmp/fovi/
-pip3 install -r requirements.txt > /dev/null 2>&1
+# Crash checker
+set -e
 
 # Directory selection (default ~/.minecraft)
-read -p "SELECT | Enter the directory you want (Press Enter to skip): " dir
+echo -n "SELECT | Enter the directory you want (Press Enter to skip): "
+read dir
 if [ -z $dir ]; then
     dir="$HOME/.minecraft"
 fi
 
+# Downloading the installer, installing python-tk and the python requirements
+download() {
+     echo "INFO | Downloading the installer..."
+     git clone https://github.com/Fabulously-Optimized/vanilla-installer /tmp/fovi
+
+     echo "INFO | Downloading the dependencies..."
+     pip install tk > /dev/null 2>&1
+     cd /tmp/fovi/
+     pip3 install -r requirements.txt > /dev/null 2>&1
+}
+
 # Moving the python scripts to the selected directory, making them executable
-echo "INFO | Installing the FO vanilla installer..."
-cd $dir/
-mkdir VanillaInstaller
-cd VanillaInstaller/
-mkdir scripts
-cd /tmp/fovi/
-mv vanilla-installer installer
-mv data installer media $dir/VanillaInstaller/scripts/
-cd $dir/VanillaInstaller/scripts/installer/
-chmod +x gui.py main.py theme.py
+install() {
+     echo "INFO | Installing the FO vanilla installer..."
+     cd $dir/
+     mkdir VanillaInstaller
+     cd VanillaInstaller/
+     mkdir scripts
+     cd /tmp/fovi/
+     mv vanilla-installer installer
+     mv data installer media $dir/VanillaInstaller/scripts/
+     cd $dir/VanillaInstaller/scripts/installer/
+     chmod +x gui.py main.py theme.py
+}
 
 # Cleaning up
-echo "INFO | Removing unnecessary files..."
-cd /tmp/
-rm -rf fovi
-cd $dir/VanillaInstaller/scripts/media/
-rm -r screenshots
+cleanup() {
+     echo "INFO | Removing unnecessary files..."
+     cd /tmp/
+     rm -rf fovi
+     cd $dir/VanillaInstaller/scripts/media/
+     rm -r screenshots
+}
 
 # Running the script
+download "$@"
+install "$@"
+cleanup "$@"
 echo "SUCCESS | Starting the script..."
 cd $dir/VanillaInstaller/scripts/installer/
 python3 gui.py
