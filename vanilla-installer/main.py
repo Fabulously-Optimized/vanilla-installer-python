6ede8d83ebb2b316a908410a28bf65a592977e3e
osfanbuff63 <91388253+osfanbuff63@users.noreply.github.com>
2022-10-21 15:48:19 -0400

    Revert main.py change

--- vanilla-installer/main.py
+++ vanilla-installer/main.py
@@ -73,7 +73,7 @@ def get_dir() -> str:
 
     try:
         path = open(PATH_FILE, encoding="utf-8").read()
-    except FileNotFoundError:
+    except OSError:
         logger.exception("No mc_path.txt found. Calling set_dir.")
         default_dir = str(
             mll.utils.get_minecraft_directory()
