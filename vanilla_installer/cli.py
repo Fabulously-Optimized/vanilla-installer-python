3156c5aeb684c9ab11523ddea31f6b38657ed573
osfanbuff63 <91388253+osfanbuff63@users.noreply.github.com>
2022-11-03 08:37:09 -0400

    ✨ Add `about` CLI command

--- vanilla_installer/cli.py
+++ vanilla_installer/cli.py
@@ -1,6 +1,8 @@
 """A CLI interface for VanillaInstaller."""
 ## Imports
 
+import webbrowser
+
 # External
 import click
 import minecraft_launcher_lib as mll
@@ -52,6 +54,28 @@ def gui():
         "GUI was closed. Usually this is intentional, if there was an error it was likely printed above."
     )
 
+@vanilla_installer.group("about", help="Shows information about the program.")
+def about():
+    pass
+
+@about.command("bug-report", help="Report a bug or crash in VanillaInstaller.")
+def bug():
+    click.echo("Press any key to open GitHub with the bug template in your web browser...")
+    user_input = input()
+    if user_input is not None:
+        webbrowser.open("https://github.com/Fabulously-Optimized/vanilla-installer/issues/new?labels=bug&template=bug.yml&title=%5BBug%5D%3A+")
+    
+
+@about.command("feature", help="Request a feature of VanillaInstaller.")
+def feature():
+    click.echo("Press any key to open GitHub with the feature request template in your web browser...")
+    user_input = input()
+    if user_input is not None:
+        webbrowser.open("https://github.com/Fabulously-Optimized/vanilla-installer/issues/new?labels=enhancement&template=enhancement.yml&title=%5BFeature+Request%5D%3A+")
+
+@about.command("licensing", help="Shows licensing details on the program.")
+def licensing():
+    click.echo("VanillaInstaller is licensed under the MIT License.\nYou may use this program and redistribute it, with or without source code. Modified works may be under a different license, as long as the copyright notice is maintained.\nFor the full text, please see https://github.com/Fabulously-Optimized/vanilla-installer/blob/main/LICENSE.md.")
 
 if __name__ == "__main__":
     vanilla_installer()
