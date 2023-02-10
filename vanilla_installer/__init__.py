3a7297d48eabcdf4a3829c3810449c6f7b86e7d4
osfanbuff63 <osfanbuff63@osfanbuff63.tech>
2023-02-10 11:37:11 -0500

    🔖 0.2.1 because i forgot to push

--- vanilla_installer/__init__.py
+++ vanilla_installer/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (C) Fabulously Optimized 2022
 # Licensed under the MIT License. The full license text can be found at https://github.com/Fabulously-Optimized/vanilla-installer/blob/main/LICENSE.md.
 """An installer of Fabulously Optimized for the vanilla launcher."""
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __license__ = "MIT License"
